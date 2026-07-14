493742b176f8aa8bce054c545a00bc14581b33b8
YongERong <yong_e-rong@s2017.sst.edu.sg>
2026-07-14 12:12:55 +0800

    chore: clean up leaked feature toggle from feat/HIS-default-state

--- pdb2pqr/config.py
+++ pdb2pqr/config.py
@@ -179,9 +179,6 @@ DONOR_ACCEPTOR_CUTOFF = 4.3
 #: Cutoff for H(D) - H(A) - A angle when acceptor already has hydrogens
 DHAHA_ANGLE_CUTOFF = 110.0
 
-#: Default state for HIS when only 1H present (HIE/HID)
-HIS_DEFAULT_STATE = "HIE"
-
 #: Avogadro's number
 AVOGADRO = 6.02214076e23
 
